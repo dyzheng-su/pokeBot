520c7ed8238387adbaad560c3fbd2d7f2fcd62b4
xzhan130-su <xzhan130@syr.edu>
2018-12-12 19:00:38 -0500

    updated at 12/12

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # Work with Python 3.6
 # Diana Zheng
 # Xueyu Zhang
+import requests
 import discord
 import random
 import asyncio
@@ -12,22 +13,64 @@ from discord.ext.commands import Bot
 
 BOT_PREFIX = ("!")
 AUTHER = 'PokeBot'
-TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-users = {}
+TOKEN = ""
 FOOTER = 'This is a footer'
-user = ''
+WILD_MON = 'XXXXXXXXXXXXXXXXXXX'
+USERS = {}
+pick_user = ''
+catch_user = ''
+select_user = ''
+base_mons = ['bulbasaur', 'charmander', 'squirtle', 'chikorita', 'cyndaquil', 'totodile', 'treecko',
+        'torchic', 'mudkip', 'turtwig', 'chimchar', 'piplup', 'snivy', 'tepig', 'oshawott', 'chespin', 'fennekin', 
+        'froakie', 'rowlet', 'litten', 'popplio']
+
+
+
+class POKE_USER:
+
+    pokemon_list = {}
+    win_rate = 0.0
+    win_times = 0
+    battle_times = 0
+    count = 0
+
+    def __init__(self, username, base_pokemon):
+        self.username = username
+        self.selected_pokemon = POKEMON(base_pokemon)
+        self.pokemon_list[base_pokemon] = self.selected_pokemon
+        self.count += 1
+
+    def select(self, pokemon):
+        if pokemon in pokemon_list:
+            self.selected_pokemon = self.pokemon_list[pokemon]
+            self.selected_pokemon.battle_percent = self.selected_pokemon.battle_times / self.battle_times
+
+
+class POKEMON:
+    level = 1
+    battle_times = 0
+    battle_percent = 0.0
+    win_times = 0
+    win_rate = 0.0
+
+    def __init__(self, name):
+        self.name = name
 
 
 client = Bot(command_prefix=BOT_PREFIX) 
 
 
+# @client.command()
+# async def battle(player):
+
+
+# @client.command()
+# async def ranking():
+
+
+
 @client.command()
 async def start():
-    base_mons = ['bulbasaur', 'charmander', 'squirtle', 'chikorita', 'cyndaquil', 'totodile', 'treecko',
-        'torchic', 'mudkip', 'turtwig', 'chimchar', 'piplup', 'snivy', 'tepig', 'oshawott', 'chespin', 'fennekin', 
-        'froakie', 'rowlet', 'litten', 'popplio'
-    ]
-    
     i=0
     base_list = ''
     while i < len(base_mons):
@@ -39,7 +82,7 @@ async def start():
         title = 'Choose a base pokemon to start your adventure!', 
         description = base_list
     )
-    embed.set_footer(text=FOOTER)
+    embed.set_footer(text="type !pick to pick your first pokemon")
     embed.set_author(name=AUTHER
         #,icon_url=
     )
@@ -48,19 +91,63 @@ async def start():
 
 
 @client.command()
-async def pick(message):
-    print(message)
+async def select(context):
+    context = context.lower()
+    user = USERS[select_user]
+    pl = user.pokemon_list
+
+    new_select_mon = pl.get(context,0)
+    if new_select_mon:
+        user.selected_pokemon = new_select_mon
+        embed = discord.Embed(
+        title = '%s select %s' % (user.username, context), 
+        description = base_list
+        )
+        embed.set_footer(text=FOOTER)
+        embed.set_author(name=AUTHER
+        #,icon_url=
+        )
+        await client.say(embed=embed)
+    else:
+        embed = discord.Embed(
+        title = '%s can not select %s, type !info to check pokemon list' % (user.username, context), 
+        description = base_list
+        )
+        embed.set_footer(text=FOOTER)
+        embed.set_author(name=AUTHER
+        #,icon_url=
+        )
+        await client.say(embed=embed)
+
 
 
 @client.command()
-async def list():
-    pokemon_list = users[user]['pokemon_list']
-    des = ''
-    for pokemon in pokemon_list:
-        des += ('%s|  level %d\n') % (pokemon['name'], pokemon['level'])
+async def pick(message):
+    mon = message.lower()
+    if mon in base_mons:
+        msg = "Good luck! %s and your pokemon %s" % (pick_user, mon)
+        new_user = POKE_USER(pick_user,mon)
+        USERS[pick_user] = new_user
+        await client.say(msg)
+    else:
+        await client.say(("%s is not a base pokemon" % mon))
+
+    while 1:
+        rand_time = random.randrange(200,500)
+        await asyncio.sleep(rand_time)
+        await wild_pokemon()
+
+@client.command()
+async def info(context):
+    user = USERS[context]
+    pl = user.pokemon_list
+    des = ('pokemons %d | total battles %d | total win %d | win percent %d\n\n\n') % (user.count, user.battle_times, user.win_times, user.win_rate)
+    for name in pl:
+        des += ('%s | level %d | battle times %d | battle percent %d | win times %d | win rate %d\n') % (pl[name].name, 
+            pl[name].level, pl[name].battle_times, pl[name].battle_percent, pl[name].win_times, pl[name].win_rate)
 
     embed = discord.Embed(
-        title = ('%s\'s pokemons') % user,
+        title = ('%s\'s infomation') % context,
         description = des
     )
 
@@ -73,10 +160,10 @@ async def list():
 
 
 @client.command()
-async def catch(context):
-    context = context.lower()
-    users[user]['pokemon_list'].append({'name': context, 'level': 0})
-    await client.say('type !list to check your new pokemons')
+async def catch():
+    new_pokemon = POKEMON(WILD_MON)
+    USERS[catch_user].pokemon_list[WILD_MON] = new_pokemon
+    await client.say('type !info to check your new pokemons')
 
     
 
@@ -131,7 +218,7 @@ async def moves(context):
         title = ('%s\'s moves' % (context.capitalize())),
         description = pokemon_moves
     )
-    embed.set_footer(text=FOOTER)
+    #embed.set_footer(text=FOOTER)
     embed.set_author(name=AUTHER,
         #,icon_url
     )
@@ -139,32 +226,60 @@ async def moves(context):
     await client.say(embed=embed)
 
 
+async def wild_pokemon():
+    global WILD_MON
+
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    response = requests.get(url)
+    data = response.json()
+    total = data['count']
+    rand = random.randrange(total)
+    wild = data['results'][rand]['name']
+
+    WILD_MON = wild
+
+    wild_mon = pb.pokemon(wild)
+    embed = discord.Embed(
+        title = wild_mon.name.capitalize(),
+        description = "A wild %s appears!!" % wild_mon.name.capitalize()
+    )
+    embed.set_footer(text="type !catch to catch this wild %s" % wild)
+    embed.set_image(url=wild_mon.sprites.front_default)
+    embed.set_author(name=AUTHER
+        #,icon_url=
+    )
+    #embed.set_thumbnail
+    embed.add_field(name='Species', value=wild_mon.species.name, inline=True)
+    #embed.add_field(name='Habitat', value=wild_mon.species.habitat.name, inline=True)
+    embed.add_field(name='Base experience', value=wild_mon.base_experience, inline=True)
+    embed.add_field(name='Id', value=wild_mon.id, inline=True)
+    for stat in wild_mon.stats:
+        embed.add_field(name=stat.stat.name.capitalize(), value=stat.effort, inline=True)
+    abilities = ''
+    for ability in wild_mon.abilities:
+        abilities += (ability.ability.name + ' | ')
+
+    embed.add_field(name='Ability', value=abilities, inline=True)
+
+    await client.say(embed=embed)
+
+
 @client.event
 async def on_message(message):
-    global user, users
+    global pick_user, catch_user, select_user
     if message.author == client.user:
         return
 
     if message.content.startswith('!pick'):
-        mon = (message.content.split())[1].lower()
-        msg = "Good luck! %s and your pokemon %s" % (message.author.name, mon)
-        pokemon_list = [{'name': mon, 'level': 0}]
-        pokemons = {'pokemon_list': pokemon_list, 'selected_pokemon': mon}
-        user = message.author.name
-        users[user] = pokemons
-        await client.send_message(message.channel, msg)
-
-    if message.content.startswith('!list'):
-        user = message.author.name
-    
+        pick_user = message.author.name
 
     if message.content.startswith('!catch'):
-        user = message.author.name
+        catch_user = message.author.name
+
+    if message.content.startswith('!select'):
+        select_user = message.author.name
 
     await client.process_commands(message)
-
-
-
 
 async def list_servers():
     await client.wait_until_ready()
@@ -172,7 +287,8 @@ async def list_servers():
         print("Current servers:")
         for server in client.servers:
             print(server.name)
-        await asyncio.sleep(600)
+        await asyncio.sleep(60)
+
 
 
 client.loop.create_task(list_servers())
